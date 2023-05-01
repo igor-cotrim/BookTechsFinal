ce847bb712a0c2d35a15bcaa2aa6e3b92c21978a
Igor Cotrim <igor.santos@verity.com.br>
2023-04-30 23:11:29 -0300

    fix: update docker-compose.yml

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,9 +3,11 @@ import { API_URL_BOOKS, API_URL_REVIWS } from "@env";
 
 const booksApi = axios.create({
   baseURL: `${API_URL_BOOKS}`,
+  // baseURL: API_URL_BOOKS,
 });
 const reviwsApi = axios.create({
   baseURL: `${API_URL_REVIWS}`,
+  // baseURL: API_URL_REVIWS,
 });
 
 export default { booksApi, reviwsApi };
